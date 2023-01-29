c7b2a78a028e6887a7eef741b7b0aaf3ff567a48
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-01-29 15:25:15 -0700

    Pemdas exercise

---
+++ tutorial/mosh/Exercises/Exercise2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Write code to swap the value of two variables.
+
+int main() {
+    double x = 10;
+    int y = 5;
+    double z = (x+10)/(3*y);
+
+    std::cout << "Value of z: " << z;
+    return 0;
+}
