419b619e54800844b4ef3287baa47da25456ffad
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-13 19:50:16 -0700

    Created folders for CSharp tutorial

--- project/2ndProject/LoginSystem.cpp
+++ project/2ndProject/LoginSystem.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 //link: https://www.youtube.com/watch?v=I_aWPGCaaFA
+
+bool isLoggedIn() {
+    cout << "";
+}
+
+int main() {
+    int choice;
+
+
+}
