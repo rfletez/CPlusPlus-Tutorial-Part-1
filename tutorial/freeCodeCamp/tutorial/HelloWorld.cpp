94dd40667f92fc19cbec0a5e099d646085ed9cab
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-09-08 12:56:04 -0700

    Simple Hello World file

---
+++ tutorial/freeCodeCamp/tutorial/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!!!" << endl;
+    cout << "Run this command: g++ -o hello HelloWorld.cpp" << endl;
+    cout << "Run it again after saving changes, and the changes will" 
+    << "be saved on the same target executable file." << endl;
+    return 0;
+}
