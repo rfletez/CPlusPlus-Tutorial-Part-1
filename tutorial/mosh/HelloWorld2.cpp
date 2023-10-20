abf2572a7e4cf3868125b978d04a808a60f9cf9e
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-10-20 11:56:04 -0700

    Modified DS_Store and created simple C++ files.

--- tutorial/mosh/HelloWorld2.cpp
+++ tutorial/mosh/HelloWorld2.cpp
@@ -8,6 +8,8 @@ int main() {
     
 
     std::cout << "Hello World!" << std::endl;
-    std::cout << "File size: " + file_size; 
+    std::cout << "File size: " << file_size; 
+
+    std::cout << "\nThis is just a test 123";
     return 0;
 }
